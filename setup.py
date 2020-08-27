58ba6769c9704f9867d1aa46ea86f4d186a1ea8f
sun1638650145 <47380395+sun1638650145@users.noreply.github.com>
2020-08-27 18:51:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="classicML",
-    version="0.3rc2",
+    version="0.3rc3",
     author="Steve Sun",
     license='Apache Software License',
     author_email="s1638650145@gmail.com",
