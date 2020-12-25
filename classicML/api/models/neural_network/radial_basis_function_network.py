979ef0a96162b8bc7d288986ad5c05f2a90769dd
Steve Sun <1638650145@qq.com>
2020-12-25 11:17:22 +0800

    update nn

--- classicML/api/models/neural_network/radial_basis_function_network.py
+++ classicML/api/models/neural_network/radial_basis_function_network.py
@@ -5,6 +5,7 @@ from classicML.backend import get_initializer
 from classicML.backend import get_optimizer
 from classicML.backend import get_loss
 from classicML.backend import get_metric
+from classicML.backend import io
 
 
 class RadialBasisFunctionNetwork(object):
@@ -20,10 +21,12 @@ class RadialBasisFunctionNetwork(object):
             径向基函数网络使用的损失函数.
         metric: str, classicML.metrics.Metric 实例, default='accuracy'
             径向基函数网络使用的评估函数.
-        parameters: numpy.ndarray,
-            径向基函数网络的参数矩阵.
+        parameters: dict of numpy.ndarray,
+            神经网络的参数矩阵.
         is_trained: bool, default=None,
-            径向基函数网络训练后将被标记为True.
+            神经网络训练后将被标记为True.
+        is_loaded: bool, default=False,
+            如果模型加载了权重将被标记为True.
     """
     def __init__(self, seed=None):
         """初始化径向基函数网络.
@@ -40,8 +43,9 @@ class RadialBasisFunctionNetwork(object):
         self.optimizer = None
         self.loss = None
         self.metric = None
-        self.parameters = None
+        self.parameters = dict()
         self.is_trained = False
+        self.is_loaded = False
 
     def compile(self, hidden_units, optimizer='rbf', loss='mse', metric='accuracy'):
         """编译径向基函数网络, 配置训练时使用的超参数.
@@ -86,8 +90,9 @@ class RadialBasisFunctionNetwork(object):
         x = np.asarray(x).astype(float)
         y = np.asarray(y).astype(int)
 
-        # 使用初始化器初始化参数
-        self.parameters = self.initializer(hidden_units=self.hidden_units)
+        # 没有使用权重文件, 则使用初始化器初始化参数.
+        if self.is_loaded is False:
+            self.parameters = self.initializer(hidden_units=self.hidden_units)
         # 使用优化器优化
         self.parameters = self.optimizer(x, y, epochs, self.parameters, verbose, self.loss, self.metric, callbacks)
         # 标记训练完成
@@ -107,10 +112,74 @@ class RadialBasisFunctionNetwork(object):
         Raises:
             ValueError: 模型没有训练的错误.
         """
-        if self.is_trained is False:
+        if self.is_trained is False and self.is_loaded is False:
             CLASSICML_LOGGER.error('模型没有训练')
             raise ValueError('你必须先进行训练')
 
         y_pred, _ = self.optimizer.forward(x, self.parameters)
 
         return y_pred
+
+    def load_weights(self, filepath):
+        """加载模型参数.
+
+        Arguments:
+            filepath: str, 权重文件加载的路径.
+
+        Raises:
+            KeyError: 模型权重加载失败.
+
+        Notes:
+            模型将不会加载关于优化器的超参数.
+        """
+        # 初始化权重文件.
+        parameters_gp = io.initialize_weights_file(filepath=filepath,
+                                                   mode='r',
+                                                   model_name='RadialBasisFunctionNetwork')
+        # 加载模型参数.
+        try:
+            compile_ds = parameters_gp['compile']
+            weights_ds = parameters_gp['weights']
+
+            self.hidden_units = compile_ds.attrs['hidden_units']
+            self.optimizer = get_optimizer(compile_ds.attrs['optimizer'])
+            self.loss = get_loss(compile_ds.attrs['loss'])
+            self.metric = get_metric(compile_ds.attrs['metric'])
+            for attr in weights_ds.attrs:
+                self.parameters[attr] = weights_ds.attrs[attr]
+            # 标记加载完成
+            self.is_loaded = True
+        except KeyError:
+            CLASSICML_LOGGER.error('模型权重加载失败, 请检查文件是否损坏')
+            raise KeyError('模型权重加载失败')
+
+    def save_weights(self, filepath):
+        """将模型权重保存为一个HDF5文件.
+
+        Arguments:
+            filepath: str, 权重文件保存的路径.
+
+        Raises:
+            TypeError: 模型权重保存失败.
+
+        Notes:
+            模型将不会保存关于优化器的超参数.
+        """
+        # 初始化权重文件.
+        parameters_gp = io.initialize_weights_file(filepath=filepath,
+                                                   mode='w',
+                                                   model_name='RadialBasisFunctionNetwork')
+        # 保存模型参数.
+        try:
+            compile_ds = parameters_gp['compile']
+            weights_ds = parameters_gp['weights']
+
+            compile_ds.attrs['hidden_units'] = self.hidden_units
+            compile_ds.attrs['optimizer'] = self.optimizer.name
+            compile_ds.attrs['loss'] = self.loss.name
+            compile_ds.attrs['metric'] = self.metric.name
+            for parameter in self.parameters:
+                weights_ds.attrs[parameter] = self.parameters[parameter]
+        except TypeError:
+            CLASSICML_LOGGER.error('模型权重保存失败, 请检查文件是否损坏')
+            raise TypeError('模型权重保存失败')
