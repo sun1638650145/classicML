3cee19f21b2a56711220b91fd2cff6ad89603055
Steve Sun <1638650145@qq.com>
2021-01-13 16:05:37 +0800

    update docs/backend/losses

--- classicML/backend/python/losses.py
+++ classicML/backend/python/losses.py
@@ -21,78 +21,8 @@ class Loss(object):
         self.name = name
 
     def __call__(self, y_pred, y_true, *args, **kwargs):
+        """函数实现."""
         raise NotImplementedError
-
-
-class MeanSquaredError(Loss):
-    """均方误差损失函数.
-    """
-    def __init__(self, name='mean_squared_error'):
-        super(MeanSquaredError, self).__init__(name=name)
-
-    def __call__(self, y_pred, y_true, *args, **kwargs):
-        """
-        Arguments:
-            y_pred: numpy.ndarray, 预测的标签.
-            y_true: numpy.ndarray, 真实的标签.
-
-        Returns:
-            当前的损失值.
-        """
-        y_shape = y_true.shape[0]
-        loss = np.sum(y_pred - y_true) ** 2 / (2 * y_shape)
-
-        return np.squeeze(loss)
-
-
-class LogLikelihood(Loss):
-    """对数似然损失函数.
-    """
-    def __init__(self, name='log_likelihood'):
-        super(LogLikelihood, self).__init__(name=name)
-
-    def __call__(self, y_true, beta, *args, **kwargs):
-        """
-        Arguments:
-            y_true: numpy.ndarray, 真实的标签.
-            beta: numpy.ndarray, 模型的参数矩阵.
-            args:
-                x_hat: numpy.ndarray, 属性的参数矩阵.
-
-        Returns:
-            当前的损失值.
-        """
-        x_hat = args[0]
-        y_true = y_true.reshape(-1, 1)
-        beta = beta.reshape(-1, 1)
-        loss = -y_true * np.matmul(x_hat, beta) + np.log(1 + np.exp(np.matmul(x_hat, beta)))
-
-        return np.sum(loss)
-
-
-class Crossentropy(Loss):
-    """交叉熵损失函数,
-    将根据标签的实际形状自动使用二分类或者多分类损失函数.
-    """
-    def __init__(self, name='crossentropy'):
-        super(Crossentropy, self).__init__(name=name)
-
-    def __call__(self, y_pred, y_true, *args, **kwargs):
-        """
-        Arguments:
-            y_pred: numpy.ndarray, 预测的标签.
-            y_true: numpy.ndarray, 真实的标签.
-
-        Returns:
-            当前的损失值.
-        """
-        if y_pred.shape[1] == 1:
-            crossentropy = BinaryCrossentropy()
-        else:
-            crossentropy = CategoricalCrossentropy()
-        loss = crossentropy(y_pred, y_true)
-
-        return loss
 
 
 class BinaryCrossentropy(Loss):
@@ -102,7 +32,8 @@ class BinaryCrossentropy(Loss):
         super(BinaryCrossentropy, self).__init__(name=name)
 
     def __call__(self, y_pred, y_true, *args, **kwargs):
-        """
+        """函数实现.
+
         Arguments:
             y_pred: numpy.ndarray, 预测的标签.
             y_true: numpy.ndarray, 真实的标签.
@@ -126,7 +57,8 @@ class CategoricalCrossentropy(Loss):
         super(CategoricalCrossentropy, self).__init__(name=name)
 
     def __call__(self, y_pred, y_true, *args, **kwargs):
-        """
+        """函数实现.
+
         Arguments:
             y_pred: numpy.ndarray, 预测的标签.
             y_true: numpy.ndarray, 真实的标签.
@@ -138,6 +70,80 @@ class CategoricalCrossentropy(Loss):
         loss = -np.sum(y_true * np.log(y_pred)) / y_shape
 
         return loss
+
+
+class Crossentropy(Loss):
+    """交叉熵损失函数,
+    将根据标签的实际形状自动使用二分类或者多分类损失函数.
+    """
+    def __init__(self, name='crossentropy'):
+        super(Crossentropy, self).__init__(name=name)
+
+    def __call__(self, y_pred, y_true, *args, **kwargs):
+        """函数实现.
+
+        Arguments:
+            y_pred: numpy.ndarray, 预测的标签.
+            y_true: numpy.ndarray, 真实的标签.
+
+        Returns:
+            当前的损失值.
+        """
+        if y_pred.shape[1] == 1:
+            crossentropy = BinaryCrossentropy()
+        else:
+            crossentropy = CategoricalCrossentropy()
+        loss = crossentropy(y_pred, y_true)
+
+        return loss
+
+
+class LogLikelihood(Loss):
+    """对数似然损失函数.
+    """
+    def __init__(self, name='log_likelihood'):
+        super(LogLikelihood, self).__init__(name=name)
+
+    def __call__(self, y_true, beta, *args, **kwargs):
+        """函数实现.
+        
+        Arguments:
+            y_true: numpy.ndarray, 真实的标签.
+            beta: numpy.ndarray, 模型的参数矩阵.
+            args:
+                x_hat: numpy.ndarray, 属性的参数矩阵.
+
+        Returns:
+            当前的损失值.
+        """
+        x_hat = args[0]
+        y_true = y_true.reshape(-1, 1)
+        beta = beta.reshape(-1, 1)
+        loss = -y_true * np.matmul(x_hat, beta) + np.log(1 + np.exp(np.matmul(x_hat, beta)))
+
+        return np.sum(loss)
+
+
+class MeanSquaredError(Loss):
+    """均方误差损失函数.
+    """
+    def __init__(self, name='mean_squared_error'):
+        super(MeanSquaredError, self).__init__(name=name)
+
+    def __call__(self, y_pred, y_true, *args, **kwargs):
+        """函数实现.
+
+        Arguments:
+            y_pred: numpy.ndarray, 预测的标签.
+            y_true: numpy.ndarray, 真实的标签.
+
+        Returns:
+            当前的损失值.
+        """
+        y_shape = y_true.shape[0]
+        loss = np.sum(y_pred - y_true) ** 2 / (2 * y_shape)
+
+        return np.squeeze(loss)
 
 
 # Aliases.
--- docs/source/backend/python/losses.md
+++ docs/source/backend/python/losses.md
@@ -2,3 +2,55 @@
 
 classicML的损失函数.
 
+## Loss
+
+损失函数的基类.
+
+```python
+cml.losses.Loss(name=None)
+```
+
+### 参数
+
+* <b>name</b>: 字符串，损失函数名称.
+
+### \__call__
+
+函数实现.
+
+```python
+__call__(y_pred, y_true, *args, **kwargs)
+```
+
+#### 异常
+
+* <b>NotImplementedError</b>: 函数没有实现.
+
+## BinaryCrossentropy
+
+二分类交叉熵损失函数.
+
+```python
+cml.losses.BinaryCrossentropy(name='binary_crossentropy')
+```
+
+### 参数
+
+* <b>name</b>: 字符串，损失函数名称.
+
+### \__call__
+
+函数实现.
+
+```python
+__call__(y_pred, y_true, *args, **kwargs)
+```
+
+#### 参数
+
+* <b>y_pred</b>: 一个 Numpy数组，预测的标签.
+* <b>y_true</b>: 一个 Numpy数组，真实的标签.
+
+#### 返回
+
+当前的损失值.
