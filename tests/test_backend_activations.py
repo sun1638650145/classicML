33339982d108230f6e32deb7451fe395e30aa931
sun1638650145 <1638650145@qq.com>
2021-01-24 11:55:27 +0800

    update test

--- tests/test_backend_activations.py
+++ tests/test_backend_activations.py
@@ -1,6 +1,11 @@
 """
     测试cc后端和默认python后端的运行结果是否一致.
     使用的随机数据采取了一些限制以更好满足测试, 但仍然不具有实际意义.
+
+    Notes:
+        - 在Python中print(0.1+0.2)输出不为0.3, activations后端引擎涉及到大量的计算梯度,
+        导致这种差异尤为明显, 但实际调试发现差异小于1e-15, 对实际结果影响在工程上可以忽略,
+        因此设置阈值为1e-15.
 """
 import numpy as np
 
@@ -17,6 +22,8 @@ from classicML.backend.python.activations import Activation as PyActivation
 from classicML.backend.python.activations import Relu as PyRelu
 from classicML.backend.python.activations import Sigmoid as PySigmoid
 from classicML.backend.python.activations import Softmax as PySoftmax
+
+THRESHOLD = 1e-15
 
 
 class TestActivation(object):
@@ -42,8 +49,8 @@ class TestActivation(object):
         py_diff = py_relu.diff(output, a)
 
         assert cc_relu.name != py_relu.name
-        assert cc_value.tolist() == py_value.tolist()
-        assert cc_diff.tolist() == py_diff.tolist()
+        assert (abs(cc_value - py_value) <= THRESHOLD).all()
+        assert (abs(cc_diff - py_diff) <= THRESHOLD).all()
 
     def test_sigmoid(self):
         z = np.random.random(size=[3, 2])
@@ -62,8 +69,8 @@ class TestActivation(object):
         py_diff = py_sigmoid.diff(output, a, y_true)
 
         assert cc_sigmoid.name != py_sigmoid.name
-        assert cc_value.tolist() == py_value.tolist()
-        assert cc_diff.tolist() == py_diff.tolist()
+        assert (abs(cc_value - py_value) <= THRESHOLD).all()
+        assert (abs(cc_diff - py_diff) <= THRESHOLD).all()
 
     def test_softmax(self):
         z = np.random.random(size=[3, 2])
@@ -81,8 +88,8 @@ class TestActivation(object):
         py_diff = py_softmax.diff(output, a)
 
         assert cc_softmax.name != py_softmax.name
-        assert cc_value.tolist() == py_value.tolist()
-        assert cc_diff.tolist() == py_diff.tolist()
+        assert (abs(cc_value - py_value) <= THRESHOLD).all()
+        assert (abs(cc_diff - py_diff) <= THRESHOLD).all()
 
     def test_instance(self):
         """测试实例化."""
