accaa246a670aa7f42ce53e165b13f0daa95e7d8
sun1638650145 <1638650145@qq.com>
2021-04-22 12:14:34 +0800

    update examples 20210422-1

--- examples/save_or_load_weights.py
+++ examples/save_or_load_weights.py
@@ -1,21 +1,22 @@
-"""这个例子将展示如何保存(加载)权重."""
-import pandas as pd
+"""
+这个例子需要 classicML 0.6.1 或以上版本.
+这个例子将展示如何保存(加载)权重.
+"""
+import tempfile
+
 import classicML as cml
 
-DATASET_PATH = './datasets/西瓜数据集alpha.csv'
+DATASET_PATH = '../datasets/西瓜数据集alpha.csv'
 
 # 读取数据
-dataframe = pd.read_csv(DATASET_PATH, index_col=0, header=0)
-x = dataframe.iloc[:, :2].values
-y = dataframe.iloc[:, 2].values
-y[y == '是'] = 1
-y[y == '否'] = 0
+ds = cml.data.Dataset()
+ds.from_csv(DATASET_PATH)
 # 生成模型
 model = cml.models.LogisticRegression(seed=2021)
 model.compile(optimizer='newton',
               loss='log_likelihood',
               metric='accuracy')
 # 训练模型
-model.fit(x, y, epochs=10, verbose=True, callbacks=None)
+model.fit(ds.x, ds.y, epochs=10, verbose=True, callbacks=None)
 # 保存权重(当你需要加载的时候, 创建模型后使用load_weights方法加载模型)
-model.save_weights('./weights.h5')
+model.save_weights(tempfile.mkstemp()[1])
