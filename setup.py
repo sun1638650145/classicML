e044f0125d9470161d6d2ab0790d4e0b5f9b1723
Steve R. Sun <47380395+sun1638650145@users.noreply.github.com>
2021-05-23 14:00:00 +0800

    support python3.6

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     cmdclass={'build_ext': build_ext},
     install_requires=[
         'h5py>=2.10.0, <=3.1.0',
-        'matplotlib>=3.3.2, <=3.4.2',
+        'matplotlib>=3.3.2, <=3.3.4',
         'numpy>=1.19.2, <=1.19.4',
         'pandas>=1.1.3, <=1.1.4',
         'psutil>=5.7.2',
