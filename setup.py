cfc9dabc485bce7b25113539fe611faf2b89242b
Steve R. Sun <1638650145@qq.com>
2021-12-12 12:04:49 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 # /path/to/eigen3/download
 if system() == 'Windows':
     EIGEN_DIR = 'c:/vcpkg/installed/x64-windows/include/eigen3'
-    LIBRARIES = ['_utils.cp37-win_amd64.lib']  # _utils.cp38-win_amd64.lib
+    LIBRARIES = ['_utils.cp37-win_amd64']  # _utils.cp38-win_amd64.lib
     LIBRARY_DIRS = sorted(glob('classicML/backend/cc/_utils/*.cc'))
 else:
     EIGEN_DIR = '/usr/local/include/eigen3'
